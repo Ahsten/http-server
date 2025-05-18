3c6aad774814067579d55df3ff10b75fed35384f
Luxe17 <hanson1717@live.com>
2025-05-18 15:20:55 -0700

    Created read_request fn to read data

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,18 @@
-pub fn main() !void {}
+const std = @import("std");
+const SocketConfig = @import("config.zig");
+const stdout = std.io.getStdOut().writer();
+const Request = @import("request.zig");
+
+pub fn main() !void {
+    const socket = try SocketConfig.Socket.init();
+    try stdout.print("Server address: {any}\n", .{socket._address});
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+
+    _ = try Request.read_request(connection, buffer[0..buffer.len]);
+    try stdout.print("{s}\n", .{buffer});
+}
